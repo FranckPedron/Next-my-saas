d03c3179c88c2fd5df0fa65b400f244a93ef39fb
FranckPedron <franckyp1+rapports_git@gmail.com>
2025-02-24 12:10:48 +0100

    Add home page

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -15,7 +15,7 @@ export default function Nav() {
                 </Link>
             </div>
             <div className="flex items-center gap-4">
-            <ThemeToggle />
+                <ThemeToggle/>
             </div>
         </nav>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
-
+'use client'
+import Image from "next/image";
+import logoApp from "@/public/logo.svg";
+import {Cursor, Typewriter} from "react-simple-typewriter";
+import ButtonProvider from "@/app/components/ButtonProvider";
 
 export default function Home() {
-  return (
-    <>
-      <h1>Hello world !</h1>
-    </>
-  );
+    return (
+        <section className="w-full h-screen flex justify-center items-center flex-col gap-2">
+            <Image src={logoApp} alt="Logo application" width={100} height={100}
+                   mb-4 className="object-contain "/>
+            <h1 className="text-4xl md:text-6xl font-black mb-2 uppercase text-center flex items-center">
+                <Typewriter typeSpeed={50} words={["Bienvenue", "Welcome", "Bienvenido"]} loop={0}/>
+                <span><Cursor/></span>
+            </h1>
+            <p></p>
+            <ButtonProvider/>
+        </section>
+    );
 }
