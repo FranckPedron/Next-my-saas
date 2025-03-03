9566955dca643fabd52d6d270870e5b38c2a296b
FranckPedron <franckyp1+rapports_git@gmail.com>
2025-03-03 22:21:29 +0100

    Add stripe

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,17 +1,37 @@
 import DashboardNav from "../components/DashboardNav";
 import ButtonSignOut from "../components/ButtonSignOut";
-import { ToastContainer, toast } from 'react-toastify';
+import {toast, ToastContainer} from 'react-toastify';
+import {getUser} from "@/lib/actionsUsers";
+import {stripe} from "@/lib/stripe";
+import {prisma} from "@/lib/db";
 
-export default async function DashboardLayout({children}:Readonly<{children:React.ReactNode}>) {
+export default async function DashboardLayout({children}: Readonly<{ children: React.ReactNode }>) {
+    const user = await getUser();
+
+    if (!user) {
+        toast.error('Veuillez vous connecter.');
+        return null;
+    }
+
+    if (!user?.stripeCustomerId) {
+        const stripeCustomer = await stripe.customers.create({
+            email: user?.email as string,
+        });
+        await prisma.user.update({
+            where: {id: user?.id as string},
+            data: {stripeCustomerId: stripeCustomer.id}
+        });
+    }
+
     return (
         <section className="max-w-[1200px] mx-auto md:flex md:items-center md:gap-4 h-screen w-full mt-2 p-2">
 
-            <DashboardNav />
+            <DashboardNav/>
 
             <div className="w-full h-full">
-                <ButtonSignOut />
+                <ButtonSignOut/>
                 {children}
-                <ToastContainer />
+                <ToastContainer/>
             </div>
         </section>
     )
